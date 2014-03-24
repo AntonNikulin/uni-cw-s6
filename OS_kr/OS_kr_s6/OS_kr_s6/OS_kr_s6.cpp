f6ace55d6a9ddd144f635d374a57137fa9fbb6f0
Anton Nikulin <dr.tallin@gmail.com>
2014-03-24 19:04:31 +0200

    boilerplate

---
+++ OS_kr/OS_kr_s6/OS_kr_s6/OS_kr_s6.cpp
@@ -0,0 +1,12 @@
+// OS_kr_s6.cpp : Defines the entry point for the console application.
+//
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout<<"hi\n";
+	system ("pause");
+	return 0;
+}
+
