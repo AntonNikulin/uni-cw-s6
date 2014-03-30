df0e859f2fab814a4dfa08746f7f407e801471b0
Anton Nikulin <dr.tallin@gmail.com>
2014-03-30 14:35:03 +0300

    ScrLang registration; readme; django-crispy-forms

---
+++ ScrLang/UniScrLangKpp/README.md
@@ -0,0 +1,4 @@
+##Req:
+*   python 2.7
+*   django 1.6
+*   django-crispy-forms 1.4
--- ScrLang/UniScrLangKpp/UniScrLangKpp/settings.py
+++ ScrLang/UniScrLangKpp/UniScrLangKpp/settings.py
@@ -127,6 +127,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'Checkpoint',
+    'crispy_forms'
 )
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
