c9dc6b9d8596852a72205cb20a78f66a9ae0b128
Anton Nikulin <dr.tallin@gmail.com>
2014-03-30 13:26:12 +0300

    ScrLang login

--- ScrLang/UniScrLangKpp/Checkpoint/views.py
+++ ScrLang/UniScrLangKpp/Checkpoint/views.py
@@ -1,4 +1,23 @@
 from django.shortcuts import render, get_object_or_404, HttpResponseRedirect
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
+from forms import RegistrationForm
+
 
 def Index(request):
     return render(request, 'base.html')
+
+def registrationPage(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = User.objects.create_user(
+                username = form.cleaned_data['username'],
+                password = form.cleaned_data['password1'],
+                email = form.cleaned_data['email']
+            )
+            return HttpResponseRedirect('/')
+        return render( request, 'registration/register.html', {"form": form,} )
+    else:
+        form = RegistrationForm()
+        return render( request, 'registration/register.html', {"form": form,} )
--- ScrLang/UniScrLangKpp/UniScrLangKpp/urls.py
+++ ScrLang/UniScrLangKpp/UniScrLangKpp/urls.py
@@ -6,7 +6,14 @@ from Checkpoint import views
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-                       url(r'^$', 'Checkpoint.views.Index', name='Index'),
+                        #authentication
+                        url(r"^login/$", "django.contrib.auth.views.login", {"template_name": "registration/login.html"}, name="login"),
+                        url(r"^logout/$", "django.contrib.auth.views.logout", {"next_page": "/"}, name= "logout"),
+                        url(r"^register/$", "Checkpoint.views.registrationPage", name="registration"),
+
+                        #custom
+                        url(r'^$', 'Checkpoint.views.Index', name='Index'),
+
     # Examples:
     # url(r'^$', 'UniScrLangKpp.views.home', name='home'),
     # url(r'^UniScrLangKpp/', include('UniScrLangKpp.foo.urls')),
