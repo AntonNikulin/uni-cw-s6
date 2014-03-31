0fe7cc8c0cb4995240a97a17a1403c6b7acae869
Anton Nikulin <dr.tallin@gmail.com>
2014-03-31 22:20:31 +0300

    ScrLang + table headers, link: createRecord

--- ScrLang/UniScrLangKpp/Checkpoint/urls.py
+++ ScrLang/UniScrLangKpp/Checkpoint/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
                         url(r'^table/$', 'Checkpoint.views.table', name='table'),
-                        url(r'^record/create/$', 'Checkpoint.views.recordCreate', name='record'),
+                        url(r'^record/create/$', 'Checkpoint.views.recordCreate', name='createRecord'),
 )
