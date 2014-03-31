5c6b34b1f64425ee07854181d9cd8dcf25d6ed38
Anton Nikulin <dr.tallin@gmail.com>
2014-03-31 19:48:30 +0300

    ScrLang + models

--- ScrLang/UniScrLangKpp/Checkpoint/models.py
+++ ScrLang/UniScrLangKpp/Checkpoint/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+# -*- coding: utf-8 -*-
+
+class Person(models.Model):
+    firstName = models.CharField(max_length=50, blank=False)
+    lastName = models.CharField(max_length=50, blank=True)
+    country = models.CharField(max_length=50, blank=True)
+
+    def __unicode__(self):
+        return self.firstName
+
+
+
+
+class Vehicle(models.Model):
+    Number = models.CharField(max_length=50, blank=False)
+    Driver = models.ForeignKey(Person, blank=True, null=True, on_delete=models.SET_NULL)
+
+    def __unicode__(self):
+        return self.Number
+
+
+class Record(models.Model):
+    vehicle = models.ForeignKey(Vehicle, blank=True, null=True, on_delete=models.SET_NULL)
+    description = models.TextField(max_length=1000, blank=True, null=True)
