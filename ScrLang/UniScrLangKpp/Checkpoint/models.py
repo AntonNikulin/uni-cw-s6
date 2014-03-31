709ce9538bba0c2397531608dcd6fc8df0c83f53
Anton Nikulin <dr.tallin@gmail.com>
2014-03-31 21:31:28 +0300

    ScrLang + oneTable model

--- ScrLang/UniScrLangKpp/Checkpoint/models.py
+++ ScrLang/UniScrLangKpp/Checkpoint/models.py
@@ -1,26 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # -*- coding: utf-8 -*-
 
-class Person(models.Model):
-    firstName = models.CharField(max_length=50, blank=False)
-    lastName = models.CharField(max_length=50, blank=True)
-    country = models.CharField(max_length=50, blank=True)
-
-    def __unicode__(self):
-        return self.firstName
-
-
-
-
-class Vehicle(models.Model):
-    Number = models.CharField(max_length=50, blank=False)
-    Driver = models.ForeignKey(Person, blank=True, null=True, on_delete=models.SET_NULL)
-
-    def __unicode__(self):
-        return self.Number
-
-
 class Record(models.Model):
-    vehicle = models.ForeignKey(Vehicle, blank=True, null=True, on_delete=models.SET_NULL)
-    description = models.TextField(max_length=1000, blank=True, null=True)
+    vehicleNumber = models.CharField(max_length=50, blank=False)
+    driver = models.CharField(max_length=50, blank=True)
+    cargo = models.CharField(max_length=50, blank=True)
+    time = models.DateTimeField(auto_now_add=True)
+    recordedBy = models.ForeignKey(User, null=True, blank=True)
+
