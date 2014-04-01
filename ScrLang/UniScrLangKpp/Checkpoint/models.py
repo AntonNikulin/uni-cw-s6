7e85b55fd2e34de2b6c0518b6a46198aec4e5cc0
Anton Nikulin <dr.tallin@gmail.com>
2014-04-01 15:56:22 +0300

    ScrLang model dest field

--- ScrLang/UniScrLangKpp/Checkpoint/models.py
+++ ScrLang/UniScrLangKpp/Checkpoint/models.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import User
 class Record(models.Model):
     vehicleNumber = models.CharField(max_length=50, blank=False)
     driver = models.CharField(max_length=50, blank=True)
+    destination = models.CharField(max_length=50, blank=True)
     cargo = models.CharField(max_length=50, blank=True)
     time = models.DateTimeField(auto_now_add=True)
     recordedBy = models.ForeignKey(User, null=True, blank=True)
