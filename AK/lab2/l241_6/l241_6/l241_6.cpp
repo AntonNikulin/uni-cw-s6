92574808c852ed925e5a668955a1a5d39161d8e1
Anton Nikulin <dr.tallin@gmail.com>
2014-04-03 22:44:59 +0300

    raise to power

--- AK/lab2/l241_6/l241_6/l241_6.cpp
+++ AK/lab2/l241_6/l241_6/l241_6.cpp
@@ -6,6 +6,19 @@
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	int sum = 0;
+	long N = 1;
+	short Num1 = 3;
+	short Num2 =2;
+	__asm{
+		mov DX,0; регистр DX пригодится в дальнейшем
+		mov AX,Num1; число которое будут возводить в степень
+		mov CX,Num2; степень числа
+		dec CX
+	a1:	mul Num1
+		loop a1
+; результат в DX:AX
+	}
 	return 0;
 }
 
