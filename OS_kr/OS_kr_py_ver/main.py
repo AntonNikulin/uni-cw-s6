3471e1cb3e835d56a8803ddbc37bf8647f40e7f4
Anton Nikulin <dr.tallin@gmail.com>
2014-04-12 16:07:31 +0300

    OS created process

--- OS_kr/OS_kr_py_ver/main.py
+++ OS_kr/OS_kr_py_ver/main.py
@@ -1,1 +1,18 @@
+import win32process
 
+
+
+si = win32process.STARTUPINFO()
+# Create a process specified by commandLine
+# Returns the handle to the new process.
+hProcess = win32process.CreateProcess(
+                      None,     # AppName
+                      "Notepad in1.txt",# Command line
+                      None,     # Process Security
+                      None,     # ThreadSecurity
+                      0,        # Inherit Handles?
+                      win32process.NORMAL_PRIORITY_CLASS,
+                      None,     # New environment
+                      None,     # Current directory
+                      si)       # startup info.
+print hProcess[0], hProcess[1]
