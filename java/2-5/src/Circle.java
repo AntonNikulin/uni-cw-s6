ceb27d2d50dd371164e5f3790a0eac9af1cfdcc7
Anton Nikulin <dr.tallin@gmail.com>
2014-04-15 19:58:05 +0300

    java circle

---
+++ java/2-5/src/Circle.java
@@ -0,0 +1,68 @@
+
+public class Circle {
+/*
+ * Helper class for 4.6 task
+ */
+	public Circle(int x, int y){
+		this.setX(x);
+		this.setY(y);
+	}
+	
+	public void moveCircle(int dx, int dy){
+		this.setX(this.getX() + dx);
+		this.setY(this.getY() + dy);
+	}
+	
+	public boolean isIntersecting(Circle c2) {
+	    double distance = distanceTo(c2);
+	    if (Math.abs((r - c2.getR())) <= distance && distance <= (r + c2.getR())) {
+	        return true;
+	    } else {
+	        return false;
+	    }
+	}
+	
+    public boolean isCircleInside(Circle c2) {
+        return distanceTo(c2) + r <= c2.getR();
+    }
+
+	
+	public double distanceTo(Circle c2){
+		return  Math.sqrt((x - c2.getX()) * (x - c2.getX()) + (y - c2.getY()) * (y - c2.getY()));
+	}
+	
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	/**
+	 * @return the y
+	 */
+	public int getY() {
+		return y;
+	}
+
+	/**
+	 * @param y the y to set
+	 */
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getR() {
+		return r;
+	}
+
+	public void setR(int r) {
+		this.r = r;
+	}
+
+	private int x;
+	private int y;
+	private int r;
+}
+
---
+++ java/2-5/src/task_46.java
@@ -0,0 +1,26 @@
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+
+public class task_46 implements DocumentListener{
+
+	
+	
+	@Override
+	public void changedUpdate(DocumentEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void insertUpdate(DocumentEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeUpdate(DocumentEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
